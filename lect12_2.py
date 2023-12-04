376b9175443c34a6b586dcb6081b407ac1ede265
sjnq <1197440299@qq.com>
2023-12-04 17:44:21 +0900

    12-04

---
+++ lect12_2.py
@@ -0,0 +1,220 @@
+#선 스타일
+#선 스타일 설정
+import matplotlib.pyplot as plt 
+
+
+#data = [2, 3, 6, 7, 10]
+#data = [1, 4, 5, 8, 9]
+#1data = [2, 4, 6, 8, 10]
+#
+#lt.plot(xdata, ydata)
+#lt.xlabel("x_data")
+#lt.ylabel("y_data")
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], "-", linestyle="solid", label="PData(km)")
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], "- -", linestyle="solid", label="PData(km)")
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], ":", linestyle="solid", label="PData(km)")
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], ".", linestyle="solid", label="PData(km)")
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], "-.", linestyle="solid", label="PData(km)")
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], ".-", linestyle="solid", label="PData(km)")
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], ".--", linestyle="solid", label="PData(km)")
+ 
+
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], linestyle="solid", label="PData(km)")
+
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], linestyle="solid", label="PData(km)")
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], linestyle="dashed", label="PData(km)")
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], linestyle="dotted", label="PData(km)")
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], linestyle="dashdot", label="PData(km)")
+
+#solid  간격
+# linestyle(0, (픽셀크기, 여백간격))
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], linestyle=(0, (1, 0)), label="PData(km)")
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], linestyle=(0, (4, 2)), label="PData(km)")
+# plt.plot([2,3,6,7,10], [1,4,5,8,9], linestyle=(0, (10, 1)), label="PData(km)")
+
+#dash
+
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], linestyle=(0, (5, 1)), label="PData(km)")
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], linestyle=(0, (5, 5)), label="PData(km)")
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], linestyle=(0, (1, 1)), label="PData(km)")
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], linestyle=(0, (1, 2)), label="PData(km)")
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], linestyle=(0, (1, 5)), label="PData(km)")
+#dashdot
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], linestyle=(0, (5, 1, 1, 3)), label="PData(km)")
+
+
+#plt.plot([1,3,5,7,9], [2,4,6,8,10], "-", color="r", label="Value(m)")
+#plt.plot([1,3,5,7,9], [2,4,6,8,10], "-", color="g", label="Value(m)")
+#plt.plot([1,3,5,7,9], [2,4,6,8,10], "-", color="b", label="Value(m)")
+#plt.plot([1,3,5,7,9], [2,4,6,8,10], "-", color="red", label="Value(m)")
+#plt.plot([1,3,5,7,9], [2,4,6,8,10], "-", color="lime", label="Value(m)")
+#plt.plot([1,3,5,7,9], [2,4,6,8,10], "-", color="C1", label="Value(m)")
+#plt.plot([1,3,5,7,9], [2,4,6,8,10], "-", color="C6", label="Value(m)")
+#plt.plot([1,3,5,7,9], [2,4,6,8,10], "-", color="#AD7334", label="Value(m)")
+# #plt.plot([1,3,5,7,9], [2,4,6,8,10], "-", color="#6988AD", label="Value(m)")
+
+#스타일
+#plt.plot([1,3,5,7,9], [2,4,6,8,10], linestyle="solid", linewidth=10, solid_capstyle="round", label="Value(m)")
+#plt.plot([1,3,5,7,9], [2,4,6,8,10], linestyle="dashed", linewidth=5, solid_capstyle="round", label="Value(m)")
+# plt.plot([1,3,5,7,9], [2,4,6,8,10], linestyle="dashed", linewidth=10, solid_capstyle="butt", label="Value(m)"
+# plt.plot([1,3,5,7,9], [2,4,6,8,10], linestyle="dashed", linewidth=10, solid_capstyle="round", label="Value(m)"
+# plt.plot([1,3,5,7,9], [2,4,6,8,10], linestyle="dashed", linewidth=10, solid_capstyle="round", label="Value(m)"
+# plt.plot([1,3,5,7,9], [2,4,6,8,10], linestyle="dashed", linewidth=10, dashed_capstyle="round", label="Value(m)"
+# plt.plot([1,3,5,7,9], [2,4,6,8,10], linestyle="dashed", linewidth=10, dashed_capstyle="round", label="Value(m)"
+
+# c=cyan d=diamond
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], "cd", label="PData(km)")
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], "bo", label="PData(km)")
+
+
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], "go-", label="PData(km)")
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], "co--", label="PData(km)")
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], "kd-.", label="PData(km)")
+
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], marker="H", label="PData(km)")
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], marker="D", label="PData(km)")
+#plt.plot([2,3,6,7,10], [1,4,5,8,9], marker="x", label="PData(km)")
+# plt.plot([2,3,6,7,10], [1,4,5,8,9], marker=11, label="PData(km)")
+# plt.plot([2,3,6,7,10], [1,4,5,8,9], marker="s", label="PData(km)")
+# plt.plot([2,3,6,7,10], [1,4,5,8,9], marker="$text$", label="PData(km)")
+# plt.plot([2,3,6,7,10], [1,4,5,8,9], marker="$y$", label="PData(km)")
+
+#그래프 영역 채우기
+#xdata = [2, 3, 6, 7, 10]
+#ydata = [1, 4, 5, 8, 9]
+#y1data = [2, 4, 6, 8, 10]
+
+#plt.plot(xdata, ydata)
+#plt.xlabel("x_data")
+#plt.ylabel("y_data")
+
+#plt.fill_between(xdata[1:4], ydata[1:4], alpha=0.5)
+#plt.fill_between(xdata[2:4], ydata[2:4], alpha=0.5)
+#plt.fill_between(xdata[:4], ydata[:4], alpha=0.5)
+#plt.fill_between(xdata[1:3], ydata[1:3], alpha=0.5)
+#plt.fill_between(xdata[1:3], ydata[1:3], alpha=0.3)
+
+# 기로 축 채우기 
+#plt.fill_betweenx(ydata[1:3], xdata[1:3], alpha=0.3)
+#plt.fill_betweenx(ydata[2:4], xdata[2:4], alpha=0.3)
+#plt.fill_betweenx(ydata[:4], xdata[:4], alpha=0.3)
+
+#plt.plot([1, 3, 5, 7 ,9]), [2, 4, 6, 8, 10]
+#plt.plot(xdata, ydata)
+
+#두 선간의 영역 채우기
+#plt.fill_between(xdata[1:4], ydata[1:4], y1data[1:4], alpha=0.5)
+#plt.fill_between(xdata[1:3], ydata[1:3], y1data[1:3], alpha=0.5)
+#plt.fill_between(xdata[:4], ydata[:4],  y1data[:4],color = "C2", alpha=0.5)
+
+#범위 지정 영역 채우기
+#plt.fill([2.9, 2.9, 7.1, 7.1], [2.5, 5.0, 8.5, 6.0], alpha=0.5)
+#plt.fill([1.9, 1.9, 3.1, 3.1], [0.5, 2.5, 8.1, 5.1], alpha=0.)
+
+
+
+"""plt.xlabel("x_data")
+plt.ylabel("y_data")
+
+
+# cok 조컬
+plt.legend(ncol = 1)
+plt.axis([0, 10, 0, 10])
+# plt.axis("scaled")"""
+
+#plt.show()
+#=======================================================================
+"""
+#dic_val = {"x_data": [2, 3, 6, 7, 10], "y_data" :[1, 4, 5, 8, 9]}
+#dic_val = {"x1_data": [1, 3, 5, 7,  9], "y1_data" :[2, 4, 6, 8, 10]}
+#
+#plt.plot("x_data", "y_data", data=dic_val, label="PData(km)")
+#plt.plot("x1_data", "y1_data", data=dic_val, label="PData(km)")
+#plt.plot([1, 4, 5, 9], [2, 4, 8, 10])
+#plt.xlabel("x-data")
+#plt.ylabel("y-data")
+
+
+#배경 설정
+#plt.grid()
+
+
+#plt.grid(axis= "x")
+
+#plt.grid(axis= "y")
+#plt.grid(axis="y", color="g")
+#plt.grid(axis="y", color="b")
+
+# 투명도 
+#plt.grid(axis="y", color="g", alpha=0.5)
+
+# 선 종류 
+#plt.grid(axis="y", color="g", alpha=0.5, linestyle="-")
+#plt.grid(axis="y", color="g", alpha=0.5, linestyle=".")
+
+#plt.grid(axis="y", color="g", alpha=0.5, linestyle="-")
+#plt.grid(axis="x", color="r", alpha=0.3, linestyle="-")
+
+#눈금표시
+plt.xticks()
+plt.yticks()
+
+plt.xticks([0,1,2,3,4,5,6,7,8,9,10])
+plt.yticks([0,1,2,3,4,5,6,7,8,9,10])
+
+plt.xticks([1, 3, 5, 7, 9, 11])
+plt.yticks([1, 3, 5, 7, 9, 11])
+
+#plt.xticks([1,2,3,4,5,6,7,8,9,10], labels=["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"])
+#plt.yticks([0,1,2,3,4,5,6,7,8,9,10,11], labels=["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"])
+
+plt.xticks([1,2,3,4,5,6,7,8,9,10], labels=["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten"])
+plt.yticks([0,1,2,3,4,5,6,7,8,9,10,11], labels=["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"])
+
+plt.tick_params(axis="x", direction="in")"""
+
+
+
+#막대그래프 그리기
+import matplotlib.pyplot as plt
+
+x_years = ['2020', '2021', '2022']
+y_data = [100, 400, 900]
+clr = ["r", "g", "b"]
+#clr = ["r", "lime", "#114DAD"]
+
+#plt.bar(x_years, y_data)
+
+#색 지정
+#plt.bar(x_years, y_data, color="g")
+#plt.bar(x_years, y_data, color="C7")
+#plt.bar(x_years, y_data, color="#114DAD")
+
+#개별 색 지정
+#plt.bar(x_years, y_data, color=clr)
+
+#너비 설정
+#plt.bar(x_years, y_data, color=clr, width=2)
+#plt.bar(x_years, y_data, color=clr, width=0.5)
+#plt.bar(x_years, y_data, color=clr, width=0.1)
+
+#막대 위치
+#plt.bar(x_years, y_data, color=clr, align="edge", width=0.5)
+#plt.bar(x_years, y_data, color=clr, align="center", width=0.3)
+
+#막대 테두리 설정
+
+#plt.bar(x_years, y_data, color=clr, align="center", edgecolor="black", width=0.5)
+#plt.bar(x_years, y_data, color=clr, align="center", edgecolor="black", linewidth=3, width=0.5)
+# # plt.bar(x_years, y_data, color=clr, align="center", edgecolor="blue", linewidth=3, width=0.5)
+
+#축 설정
+#plt.yticks([100, 200, 300, 400, 500, 600, 900])
+
+#수평 그래프 그리기
+#plt.barh(x_years, y_data)
+
+#용션
+plt.barh(x_years, y_data, color=clr, align="center", edgecolor="black", linewidth=3, height=0.3)
+
+plt.show()
