96774820f821de4bd820a1293b0f6d6e2e1bca35
sjnq <1197440299@qq.com>
2023-12-04 15:33:30 +0900

    12-4-2.2

---
+++ lect2_2.py
@@ -0,0 +1,151 @@
+"""my_var = 10
+print(my_var)  
+
+my_list = [1, 2, 3]
+print(my_list)
+print(*my_list)
+
+# 
+my_int = 10
+
+# 
+my_float = 3.14
+
+# 복소수
+my_complex = 3 + 4j
+
+my_character = 'A'
+my_char = "@"
+print(my_char)
+
+my_string = 'Hello, World!'
+str_name = "python"
+
+my_bool = True
+bFlag = False
+print(bFlag)
+ """
+
+"""my_list = [10, 3, 8, 9, 42, "hello"]
+print(my_list[3])
+
+my_list[3] = "t"
+print(*my_list)
+ """
+
+"""#print(my_list[3] - my_list[5])
+
+slice_ls = my_list[2:]
+print(slice_ls)
+ """
+
+#my_list = [10, 3, 8, 9, 42, "hello", [5, 4, 2, "world"]]
+#my_list = [10, 3, 8, 9, 42]
+#my_list = ['c', 'z', 'a']
+"""my_list = ['python', 'hello', 'apple']
+
+#my_list.insert(20, "e")
+my_list.sort()
+#print(res)
+
+print(my_list)
+"""
+#
+"""
+my_tup = (4, 2, 6, 1, "py", "w")
+
+print(my_tup.__len__())
+
+#my_tup[2] = 9
+
+print(my_tup.count(6))
+"""
+"""
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+
+my_item = my_dict.items()
+
+print(my_item)
+"""
+
+"""
+idx = ("Key1", "key2", "Key3")
+
+dicts = dict.fromkeys(idx, "0")
+
+print(dicts)
+"""
+
+
+"""
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+dicData = {"name" : "python", "number" : 23564897}
+
+print(my_dict["key1"])
+"""
+
+"""
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+
+my_str = my_dict.get("key2")
+
+print(my_str)
+"""
+
+"""
+my_dict = {'key1': 'value1', 'key2': 'value2'}
+
+print(my_dict.pop("key1"))
+
+print(my_dict)
+"""
+
+"""
+my_dict = {'key1': 'value1', 'key2': 'value2'} 
+
+dicts = my_dict.copy()
+
+print(dicts)
+
+print(my_dict)
+"""
+
+"""
+my_dict = {'key1': 'value1', 'key2': 'value2'} 
+dicData = {"name" : "python", "number" : 23564897}
+
+my_dict["key3"] = "value3"
+
+print(my_dict)
+"""
+
+"""
+my_dict = {'key1': 'value1', 'key2': 'value2'} 
+
+my_dict.setdefault("key3")
+
+print(my_dict)
+"""
+
+"""
+my_dict = {'key1': 'value1', 'key2': 'value2'} 
+
+my_doct.update({"key1" : "v4"})
+
+print(my_dict)
+"""
+
+"""
+my_dict = {'key1': 'value1', 'key2': 'value2'} 
+
+del my_dict["key2"]
+
+print(my_dict)
+"""
+
+
+my_dict = {'key1': 'value1', 'key2': 'value2'} 
+
+del my_dict["key2"]
+
+print(my_dict)
